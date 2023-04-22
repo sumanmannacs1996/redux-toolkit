17488e3bce79643f7df0d69664755205648ef0b5
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2023-04-23 01:30:44 +0530

    Learn redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import CakeClassView from './components/CakeClassView';
+import ClassUserContainer from './components/ClassUserContainer';
+import IcecreamClassView from './components/IcecreamClassView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CakeClassView />
+      <IcecreamClassView />
+      <ClassUserContainer />
     </div>
   );
 }
